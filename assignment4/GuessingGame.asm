4a2e4f42b8608d5db62cea628c31be22769ac974
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2023-02-25 21:44:39 -0800

    Checks for valid input when valid input is 0

--- assignment4/GuessingGame.asm
+++ assignment4/GuessingGame.asm
@@ -1,17 +1,25 @@
 ; Caleb Krauter
 ; Program description !
           
-            .ORIG           x3000           ; Start at orgin x3000
-Loop        LEA             R0, Prompt      ; Ask for input    
-            PUTS                            ; display the prompt
-            GETC                            ; read a char into R0
-            OUT                             ; write the char from R0 to the monitor
-            BR              Loop                
+                .ORIG               x3000               ; Start at orgin x3000
+Loop            LEA                 R0, Prompt          ; Ask for input    
+                PUTS                                    ; display the prompt
+                GETC                                    ; read a char into R0
+                OUT                                     ; write the char from R0 to the monitor
+                BR                  CheckValidity 
+                LEA                 R0, Error           ; Reference error in R0.
+                PUTS                                    ; Display the error.
 
+
+CheckValidity   LD                  R1, Valid1          ; Load a valid value into R1
+                ADD                 R0, R0, R1          ; Add R0 and R1 together
+                BRz                 Loop                ; If R0 + R1 and #0 = 0, continue the loop
+                LEA                 R0, Error
+                PUTS
             
-            HALT
-Prompt      .STRINGZ        "\nGuess a number 0 to 9: "
-            
-            
-            .END
+END             HALT
+Prompt          .STRINGZ            "\nGuess a number 0 to 9: "
+Error           .STRINGZ            "\nInvalid input."            
+Valid1          .FILL               #-48              ; value for ASCII character 0.    
+                .END                
             
