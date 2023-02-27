2e056e6b07c6dc3f481dd079a405a8bf48ace936
Caleb Krauter <42187569+calebkrauter@users.noreply.github.com>
2023-02-26 22:40:35 -0800

    Check a valid range and end if input is invalid

--- assignment4/GuessingGame.asm
+++ assignment4/GuessingGame.asm
@@ -10,22 +10,27 @@ Loop            LEA                 R0, Prompt          ; Ask for input
                 ; if MinValid + (-input) = 0 or is neg, then it meets the MinValid requirement.
         
                 
-                LD                  R1, MinValid ; LD into R1 the minValid value
+MinBoundCheck   LD                  R1, MinValid ; LD into R1 the minValid value
                 NOT                 R3, R0          ; Negate R0's value          
                 ADD                 R2, R3, R1      ; R1 - R0 (ex: min(x0030 = 0) - in(x002F = some value) = pos, if in is x0030 then it's zero, if it is greater than x0030 it is neg, (valid range for min is when input sum = nz))      
-                BRn                 Loop
-                BR                  END
-                ;BRz                  Loop 
+                BRn                 MaxBoundCheck            ; Value is above the
+                BRz                 END
+                BRp                 END
+
                 
-;CheckMax         ;LEA                 R0, Error
- ;               PUTS
-   ;             GETC
-   ;             OUT
-   ;             BR                  Loop
+MaxBoundCheck   LD                  R4, MaxValid ; LD into R4, maxV
+                ;NOT                 R3, R3
+                ADD                 R5, R3, R4   ; R0's value is already negated, Add max with -INPUT 
+                BRn                 END         ; IF 0, then good, if sum neg then good
+                BRz                 END
+                BRp                 Loop
+
                
           
 END             HALT
 Prompt          .STRINGZ            "\nGuess a number 0 to 9: "
 Error           .STRINGZ            "\nInvalid input."            
 MinValid          .FILL               x0030              ; value for ASCII character 0.    
+MaxValid          .FILL               x0039              ; value for ASCII character 0.    
+
                 .END                
