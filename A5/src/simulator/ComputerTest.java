ae95b0ae2c1cb851c5e6694393f747bc1cf7b177
Caleb Krauter <krautercaleb@gmail.com>
2023-03-10 13:13:57 -0800

    Complete Not instruction and unit test pass

---
+++ A5/src/simulator/ComputerTest.java
@@ -0,0 +1,155 @@
+/*
+ * Unit tests for the Computer class. 
+ */
+
+package simulator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Alan Fowler
+ * @version 1.3
+ */
+class ComputerTest {
+	
+	// An instance of the Computer class to use in the tests.
+	private Computer myComputer;
+
+	@BeforeEach
+	void setUp() {
+		myComputer = new Computer();
+	}
+	
+	
+	
+	
+	/*
+	 * NOTE:
+	 * Programs in unit tests should ideally have one instruction per line
+	 * with a comment for each line.
+	 */
+
+	/**
+	 * Test method for {@link simulator.Computer#executeBranch()}.
+	 */
+	@Test
+	void testExecuteBranch() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link simulator.Computer#executeLoad()}.
+	 */
+	@Test
+	void testExecuteLoad() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link simulator.Computer#executeAnd()}.
+	 */
+	@Test
+	void testExecuteAnd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link simulator.Computer#executeNot()}.
+	 */
+	@Test
+	void testExecuteNot5() {
+	
+		//myComputer.display();
+		
+		// NOTE: R5 contains #5 initially when the Computer is instantiated
+		// So, iF we execute R4 <- NOT R5, then R4 should contain 1111 1111 1111 1010    (-6)
+		// AND CC should be 100
+		
+		String program[] = {
+			"1001100101111111",    // R4 <- NOT R5
+			"1111000000100101"     // TRAP - vector x25 - HALT
+		};
+		
+		myComputer.loadMachineCode(program);
+		myComputer.execute();
+		
+		assertEquals(-6, myComputer.getRegisters()[4].get2sCompValue());
+		
+		// Check that CC was set correctly
+		BitString expectedCC = new BitString();
+		expectedCC.setBits("100".toCharArray());
+		assertEquals(expectedCC.get2sCompValue(), myComputer.getCC().get2sCompValue());
+		
+		//myComputer.display();
+	}
+	
+	/**
+	 * Test method for {@link simulator.Computer#executeAdd()}. <br>
+	 * Computes 2 + 2. R0 <- R2 + R2
+	 */
+	@Test
+	void testExecuteAddR2PlusR2() {
+		
+		String[] program =
+			{"0001000010000010",  // R0 <- R2 + R2 (#4)
+		     "1111000000100101"}; // HALT
+		
+		myComputer.loadMachineCode(program);
+		myComputer.execute();
+
+		assertEquals(4, myComputer.getRegisters()[0].get2sCompValue());
+		
+		// Check that CC was set correctly
+		BitString expectedCC = new BitString();
+		expectedCC.setBits("001".toCharArray());
+		assertEquals(expectedCC.get2sCompValue(), myComputer.getCC().get2sCompValue());
+	}
+	
+	/**
+	 * Test method for {@link simulator.Computer#executeAdd()}. <br>
+	 * Computes 2 + 3. R0 <- R2 + #3
+	 */
+	@Test
+	void testExecuteAddR2PlusImm3() {
+		
+		String[] program =
+			{"0001000010100011",  // R0 <- R2 + #3
+		     "1111000000100101"}; // HALT
+		
+		myComputer.loadMachineCode(program);
+		myComputer.execute();
+
+		assertEquals(5, myComputer.getRegisters()[0].get2sCompValue());
+		
+		// Check that CC was set correctly
+		BitString expectedCC = new BitString();
+		expectedCC.setBits("001".toCharArray());
+		assertEquals(expectedCC.get2sCompValue(), myComputer.getCC().get2sCompValue());
+	}
+	
+	/**
+	 * Test method for {@link simulator.Computer#executeAdd()}. <br>
+	 * Computes 2 - 3. R0 <- R2 + #-3
+	 */
+	@Test
+	void testExecuteAddR2PlusImmNeg3() {
+		
+		String[] program =
+			{"0001000010111101",  // R0 <- R2 + #-3
+		     "1111000000100101"}; // HALT
+		
+		myComputer.loadMachineCode(program);
+		myComputer.execute();
+
+		assertEquals(-1, myComputer.getRegisters()[0].get2sCompValue());
+		
+		// Check that CC was set correctly
+		BitString expectedCC = new BitString();
+		expectedCC.setBits("100".toCharArray());
+		assertEquals(expectedCC.get2sCompValue(), myComputer.getCC().get2sCompValue());
+	}
+
+}
---
+++ A5/src/simulator/Simulator.java
@@ -0,0 +1,103 @@
+package simulator;
+
+/**
+ * The Simulator class is used to load and execute all of the instructions in a machine code program.
+ * A sample machine code program is provided which outputs the characters 9 to 1 to the console.
+ *  
+ * @author mmuppa
+ * @author acfowler
+ * @version 1.2
+ */
+public class Simulator {
+
+	public static void main(String[] args) {
+
+		Computer myComputer;
+
+		/************************************** */
+		/** The next variable - "program" */
+		/** allows someone using the simulator (such as a grader) */
+		/** to decide what program will be simulated. */
+		/** The simulation must load and execute */
+		/** instructions found in the "program" array. */
+		/**
+		/** Recompile your program without further changes */
+		/** and see the simulator load and execute the new program. */
+		/************************************** */
+		
+		/*
+		 * To run the machine code program shown below in the actual LC3 simulator we need to append the following
+		 * to the top of the program:
+		 * 
+		 * "0011000000000000", // Use address x3000 as the start location in memory for the program
+		 */
+
+		String program[] = {	
+			"0010000000001000",  // LD into R0 x39 which is ASCII 9
+			"0010001000001000",  // LD into R1 x-30
+			"0001010000000001",  // ADD R2 <- R0 + R1 ; #9 ; R2 is the counter
+			"0000010000000100",  // BR if zero skip down to code after the loop
+			"1111000000100001",  // TRAP - vector x21 - OUT R0
+			"0001000000111111",  // ADD - decrement R0 - the character
+			"0001010010111111",  // ADD - decrement R2 - the counter
+			"0000111111111011",  // BR - Loop back
+			"1111000000100101",  // TRAP - vector x25 - HALT
+			"0000000000111001",  // x39
+			"1111111111010000"}; // x-30
+
+		/*
+		 * This is the assembly program version of the binary
+		 * program shown above. 
+		 * 		 .ORIG x3000
+		 * 
+		 * 		 LD R0 START 
+		 * 		 LD R1 END 
+		 *  	 ADD R2 R0 R1 
+		 * TOP   BRZ DONE 
+		 * 		 OUT 
+		 * 		 ADD R0 R0 -1
+		 *       ADD R2 R2 -1
+		 * 		 BRNZP TOP 
+		 * DONE  HALT
+		 * 
+		 * START .FILL x39 
+		 * END   .FILL x-30
+		 * 
+		 * 		 .END
+		 */
+		
+		
+//		// Here is another program for testing:
+//		String program[] = {
+//		//A program that uses a loop to add 1 + 2 + 3 + 4 + 5 and save the result (15) in RO
+//				"0101000000100000",   //; clear R0 - R0 will be the SUM
+//				"0101001001100000",   //; clear R1 - R1 will be the loop counter
+//				"0001001001100101",   //; R1 <- R1 + 5 : set the loop counter to 5 - result = 15
+//				"0001000000000001",   //; R0 <- R0 + R1 ; add the loop counter value to the sum
+//				"0001001001111111",	  //; decrement the counter
+//				"0000001111111101",   //; BRnzp do it again if the counter is not yet zero
+//				"1111000000100101"};  //; halt - TRAP with vector x25
+		
+		
+		
+		
+		
+		myComputer = new Computer();
+		
+		/* Show the initial configuration of the computer. */
+		//myComputer.display();
+		
+		myComputer.loadMachineCode(program);
+
+		/* Execute the program. */
+		/* During execution, the only output to the screen should be */
+		/* the result of executing OUT. */
+
+		myComputer.execute();
+		
+		/* Show the final configuration of the computer. */
+		//System.out.println();
+		//myComputer.display();
+	}
+
+}
